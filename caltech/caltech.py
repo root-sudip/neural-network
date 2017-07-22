38163fd67920e66246498504b708eb35ef4f4a21
Sudip Das <d.sudip47@gmail.com>
2017-07-22 22:19:17 +0530

    updated

--- caltech/caltech.py
+++ caltech/caltech.py
@@ -31,13 +31,13 @@ from os import listdir
 
 class mlp:
 	def __init__(self,no_epoch):
-		print('mlp ceated...')
+		print('cnn ceated...')
 		self.no_epoch = no_epoch
 
-	def load_data(self,path_name,samples):
+	def load_data(self,path_name,samples=None):
 
 		self.all_array_list = []
-		self.Y_train = np.zeros([2121])
+		self.Y_train = np.zeros([964])
 		j = 0 
 		for dirName, subdirList, fileList in os.walk(path_name):
 			i = 0 
@@ -53,15 +53,15 @@ class mlp:
 						self.Y_train[j] = i	
 						j = j + 1
 						num_of_image = num_of_image + 1
-						if num_of_image < 40:
-							num_of_image = num_of_image + 1
-						else: 
-							break
+						# if num_of_image < 40:
+						#num_of_image = num_of_image + 1
+						# else: 
+						# 	break
 				print('sub dir : => ',subDir,'Number of Image => ',num_of_image,'label => ',i)
 				i = i + 1
 		self.array_list_l = np.asarray(self.all_array_list)
 		print('Total number of image => ',j)
-		self.X_train = np.reshape(self.array_list_l,(2121,32,32,3))
+		self.X_train = np.reshape(self.array_list_l,(964,32,32,3))
 		print('one_hot : ',self.Y_train)
 		print('features_array : ',self.X_train.shape)
 
@@ -70,30 +70,39 @@ class mlp:
 
 	def create_model(self):
 		self.Y_train = np_utils.to_categorical(self.Y_train)
+		print('one_hot : ',self.Y_train)
+
 		self.model = Sequential()
-		self.model.add(Conv2D(10, (10, 10), padding='same',
+		self.model.add(Conv2D(512, (5, 5), padding='same',
                  input_shape=self.X_train.shape[1:]))
 	
 		self.model.add(Activation('sigmoid'))
-		self.model.add(MaxPooling2D(pool_size=(2, 2)))
-	
-
-		self.model.add(Conv2D(12, (10, 10), padding='same'))
-		self.model.add(Activation('sigmoid'))
-	
-		self.model.add(MaxPooling2D(pool_size=(2, 2)))
-	
+		self.model.add(MaxPooling2D(pool_size=(3, 3)))
 
 
-		self.model.add(Conv2D(14, (10, 10), padding='same'))
-		self.model.add(Activation('sigmoid'))
-	
-		self.model.add(MaxPooling2D(pool_size=(2, 2)))
+		#self.model.add(Conv2D(512, (3, 3), padding='same'))
+		#self.model.add(Activation('sigmoid'))
+		#self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+		# self.model.add(Conv2D(256, (8, 8), padding='same'))
+		# self.model.add(Activation('sigmoid'))
+		# self.model.add(MaxPooling2D(pool_size=(16, 16)))
+
+
 		
 
-
+		# self.model.add(Conv2D(256, (8, 2), paddi8g='same'))
+		# self.model.add(Activation('sigmoid'))
+		# self.model.add(MaxPooling2D(pool_size=(16, 16)))
+		
 		self.model.add(Flatten())
-		self.model.add(Dense(102))
+
+		self.model.add(Dense(512))
+		self.model.add(Activation('sigmoid'))
+		
+		
+		
+		self.model.add(Dense(17))
 		
 		self.model.add(Activation('softmax'))
 
@@ -124,26 +133,43 @@ class mlp:
 		self.model = model_from_json(self.model)
 		self.model.load_weights("model_cnn.h5")
 
-	def test_model(self,filename,no_samples):
-	
-		self.test_list = []
-		self.X_test = np.asarray(Image.open(filename).resize((32,32), Image.ANTIALIAS))
-		self.test_list.append(self.X_test)
-		self.X_test = np.asarray(self.test_list)
+	def test_model(self,path_name,no_samples=None,label=None):
+
 
 		self.Y_test = np.zeros([no_samples])
-		self.Y_test[0] = 11
+		self.all_image_array = []
+		j = 0 
+
+		for fname in os.listdir(path_name):
+			print('File Name : ',fname)
+			self.test_img_array = np.asarray(Image.open(path_name+'/'+fname).resize((32,32), Image.ANTIALIAS))
+			# print(self.img_array.shape)
+			if self.test_img_array.ndim == 3:
+				self.all_image_array.append(self.test_img_array)
+				self.Y_test[j] = label	
+				j = j + 1
+
 	
-		self.classes = self.model.predict_classes(self.X_test, batch_size=120)
+		# self.test_list = []
+		# self.X_test = np.asarray(Image.open(filename).resize((32,32), Image.ANTIALIAS))
+		# self.test_list.append(self.X_test)
+		# self.X_test = np.asarray(self.test_list)
+
+		# self.Y_test = np.zeros([no_samples])
+		# self.Y_test[0] = label
+
+		self.X_test = np.asarray(self.all_image_array)
+	
+		self.classes = self.model.predict_classes(self.X_test, batch_size=1)
 
 
 		self.test_dim = self.Y_test.shape
 		print('Test dimention : ',self.test_dim)
-		self.accuration = np.sum(self.classes == self.Y_test)/1 * 100
+		self.accuration = np.sum(self.classes == self.Y_test)/no_samples * 100
 
 		print ('Test Accuration : ',str(self.accuration),'%')
 		print ('Prediction :',self.classes)
-		print ('Target :',np.asarray(self.Y_test,dtype="int32"))
+		print ('Target     :',np.asarray(self.Y_test,dtype="int32"))
 
 
 def completer(text, state):
@@ -157,16 +183,36 @@ def completer(text, state):
 readline.set_completer(completer)
 readline.parse_and_bind("tab: complete")
 
-ob = mlp(50)
+ob = mlp(150)
 
 if sys.argv[1] == 'test':
 	print('Trying to predict ...')
 	ob.load_model()
-	ob.test_model('image_0012.jpg',1)
+	ob.test_model('test',no_samples=10,label=5)
 elif sys.argv[1] == 'train_model':
 	ob.load_data('101_ObjectCategories',300)
 	ob.create_model()
 	ob.train_model()
 	ob.save_model()
 else:
-	print('You should write the argv parameter.')
+	print('You should write the argv parameters.')
+
+
+
+# sub dir : =>  accordion Number of Image =>  49 label =>  0
+# sub dir : =>  butterfly Number of Image =>  86 label =>  1
+# sub dir : =>  brain Number of Image =>  84 label =>  2
+# sub dir : =>  camera Number of Image =>  47 label =>  3
+# sub dir : =>  bass Number of Image =>  50 label =>  4
+# sub dir : =>  buddha Number of Image =>  81 label =>  5
+# sub dir : =>  beaver Number of Image =>  45 label =>  6
+# sub dir : =>  binocular Number of Image =>  32 label =>  7
+# sub dir : =>  cellphone Number of Image =>  58 label =>  8
+# sub dir : =>  barrel Number of Image =>  45 label =>  9
+# sub dir : =>  brontosaurus Number of Image =>  38 label =>  10
+# sub dir : =>  cannon Number of Image =>  40 label =>  11
+# sub dir : =>  anchor Number of Image =>  41 label =>  12
+# sub dir : =>  chair Number of Image =>  61 label =>  13
+# sub dir : =>  bonsai Number of Image =>  128 label =>  14
+# sub dir : =>  ceiling_fan Number of Image =>  45 label =>  15
+# sub dir : =>  ant Number of Image =>  34 label =>  16
