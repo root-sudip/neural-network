ba6145c7d1e3e8982f6c0177acf4696aa2dd8782
root-sudip <d.sudip47@gmail.com>
2017-08-17 17:08:08 +0530

    rotate

---
+++ rotate/rotate.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import os
+
+path_list = ['set0/','set1/']
+
+
+for dir_name in path_list:
+	#print(dir_name)
+	for file in os.listdir(dir_name):
+		if file.endswith('.png'):
+			img = Image.open(dir_name+file).rotate(120,expand=True)
+			img.save('rotate/'+dir_name+'120/'+file)
+			print(file)
