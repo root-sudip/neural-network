2c194b6cfb71be614649f78d27930adec4607ec0
root-sudip <d.sudip47@gmail.com>
2017-08-18 17:57:57 +0530

    cropped caltech pedestrain data

---
+++ pedestrian data unpack code/croped.py
@@ -0,0 +1,63 @@
+import os
+import re
+import json
+import glob
+import cv2 as cv
+from collections import defaultdict
+
+from PIL import Image
+
+annotations = json.load(open('annotations.json'))
+
+out_dir = 'data/plots'
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+img_fns = defaultdict(dict)
+
+for fn in sorted(glob.glob('images/*.png')):
+    set_name = re.search('(set[0-9]+)', fn).groups()[0]
+    img_fns[set_name] = defaultdict(dict)
+
+for fn in sorted(glob.glob('images/*.png')):
+    set_name = re.search('(set[0-9]+)', fn).groups()[0]
+    video_name = re.search('(V[0-9]+)', fn).groups()[0]
+    img_fns[set_name][video_name] = []
+
+for fn in sorted(glob.glob('images/*.png')):
+    set_name = re.search('(set[0-9]+)', fn).groups()[0]
+    video_name = re.search('(V[0-9]+)', fn).groups()[0]
+    n_frame = re.search('_([0-9]+)\.png', fn).groups()[0]
+    img_fns[set_name][video_name].append((int(n_frame), fn))
+
+n_objects = 0
+
+k_n = 0
+for set_name in sorted(img_fns.keys()):
+    for video_name in sorted(img_fns[set_name].keys()):
+        wri = cv.VideoWriter(
+            'data/plots/{}_{}.avi'.format(set_name, video_name),
+            cv.cv.CV_FOURCC(*'XVID'), 30, (640, 480))
+        for frame_i, fn in sorted(img_fns[set_name][video_name]):
+            img = cv.imread(fn)
+            if str(frame_i) in annotations[set_name][video_name]['frames']:
+                data = annotations[set_name][
+                    video_name]['frames'][str(frame_i)]
+                for datum in data:
+                    x, y, w, h = [int(v) for v in datum['pos']]
+                    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                    ###
+                    try:
+                        img_pil = Image.fromarray(img)
+                        crop = img_pil.crop((x,y,x+w,y+h))
+                        crop.save('dumped/'+str(k_n)+'.png')
+                        k_n = k_n + 1
+                    except SystemError:
+                        pass
+
+                    ###
+                    n_objects += 1
+                wri.write(img)
+        wri.release()
+        print(set_name, video_name)
+print(n_objects)
