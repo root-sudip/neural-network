db5b3ec86cbc38939686042d42781cd6872a09b6
root-sudip <d.sudip47@gmail.com>
2017-10-26 21:41:35 +0530

    sliding_window_for_network

--- caltech_pedestrian/sliding_window_for_network.py
+++ caltech_pedestrian/sliding_window_for_network.py
@@ -1,4 +1,4 @@
-def test_model(self,filename,frame_size, strides):
+	def test_model(self,filename,frame_size, strides):
 
 		image = cv.imread(filename)
 		(winW, winH) = (128, 128)
@@ -19,7 +19,8 @@ def test_model(self,filename,frame_size, strides):
 				croped_image =  np.reshape(np.asarray(Image.fromarray(window).resize((32,32), Image.ANTIALIAS)),(1,32,32,3))
 				classes = self.model.predict_classes(croped_image, batch_size=1)
 				if classes == 1:
-		 			cv.rectangle(image, (x, y), (x + winW, y + winH), (255, 0, 0), 1)
+					print('confidence value : ',classes[1])
+					cv.rectangle(image, (x, y), (x + winW, y + winH), (255, 0, 0), 1)
 				else:
 					#cv.rectangle(img, (i, j), (i + frame_size[0], j + frame_size[1]), (0, 0, 255), 1)
 					pass
